bedbcdb6db97ef729fe9d63468924cfec7bfa4dc
Hadis Sulejmani <hadissulejmani@outlook.com>
2019-08-27 21:36:38 +0200

    Build folder

---
+++ build/precache-manifest.f46fed966ae8167c1e9afc8f1d0cac5c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01aaafcd4e65b880537c0248f42334e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d1d81b3fb2e7ae69e05",
+    "url": "/static/css/main.07216b4d.chunk.css"
+  },
+  {
+    "revision": "d65ba0898b560f06d74f",
+    "url": "/static/js/2.f5792905.chunk.js"
+  },
+  {
+    "revision": "4d1d81b3fb2e7ae69e05",
+    "url": "/static/js/main.9d9a2cb8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3aba56daf4cdb74d583c3e28b43b9744",
+    "url": "/static/media/email.3aba56da.png"
+  },
+  {
+    "revision": "85ad6dc509e0f2b8060f0758ffa6f774",
+    "url": "/static/media/heroImage.85ad6dc5.png"
+  },
+  {
+    "revision": "7df97da956c1ede16c54f66bd769af4d",
+    "url": "/static/media/location.7df97da9.png"
+  },
+  {
+    "revision": "3dba69b4c1a9077b9182dd8b1c70b2ac",
+    "url": "/static/media/logo.3dba69b4.png"
+  },
+  {
+    "revision": "cea36efd906b958bff65014c28df75a7",
+    "url": "/static/media/phone.cea36efd.png"
+  }
+]);
